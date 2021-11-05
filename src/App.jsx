e4d8e8299f0fea5c9294fddd56f1713b92e694b2
Shawn Tung <shawn_tung@askey.com>
2021-11-05 17:25:42 +0800

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,26 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [data, setData] = useState('')
+  function getData(){
+    axios.get('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON')
+    .then(function(response){
+      setData(response.data)
+      console.log(response.data);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+    .then(function(){
+      console.log('a');
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>axios test</h1>
+      <button onClick={getData}>抓資料</button>
     </div>
   )
 }
