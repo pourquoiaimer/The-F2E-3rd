c363939a7166e81d1094d22385e68ee921e0f96e
Shawn Tung <shawn_tung@askey.com>
2021-11-05 18:30:31 +0800

    s

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,30 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState('')
-  function getData(){
-    axios.get('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON')
-    .then(function(response){
-      setData(response.data)
+  const userRequest = axios.create({
+    baseURL:'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON',
+  })
+  let url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON'
+  const getDatas = (url)=>{
+    console.log(url);
+    userRequest.get(url).then(function(response){
       console.log(response.data);
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-    .then(function(){
-      console.log('a');
-    })
+    }).catch(function(){})
+  }
+
+  function getData() {
+    axios.get('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON')
+      .then(function (response) {
+        setData(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
   return (
     <div className="App">
       <h1>axios test</h1>
-      <button onClick={getData}>抓資料</button>
+      <button onClick={()=>{getDatas(url)}}>抓資料</button>
     </div>
   )
 }
