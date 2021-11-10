9a22c40b79ae6751a46cfb41d97843092c259d54
Shawn Tung <shawn_tung@askey.com>
2021-11-10 18:35:39 +0800

    1110ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,14 +12,28 @@ const Home = () => {
     // }
     return (
         <div className="main_box">
-            <div className="bg">
-            </div>
-            <div className="title">
-                <h1 >
-                    冒險與發現
-                </h1>
-            </div>
+            <div className="homepage">
+                <div className="bg_mask">
+                </div>
+                <div className="bg">
+                </div>
+                <div className="homepage_content">
+                    <h1>冒險與發現</h1>
+                    <hr />
+                    <h2>Discover</h2>
+                    <div>
+                        <select className="search_area" name="area" >
+                            <option value='null'>請選擇區域</option>
+                            <option value='taipei'>台北</option>
+                            <option value='new taipei'>新北</option>
+                        </select>
+                        <input className="search_input" value='' placeholder="輸入關鍵字">
+                        </input>
+                        <button className="search_btn">搜尋</button>
+                    </div>
+                </div>
 
+            </div>
         </div>
     )
 }
